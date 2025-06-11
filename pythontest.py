25fd274363590aca7f4f8026bc39cfe2146aef18
Alec Ewe <alec.k.ewe@gmail.com>
2025-06-11 09:00:31 -0400

    This is an update to the python test file in which a multiplication function was added with a test

--- pythontest.py
+++ pythontest.py
@@ -1,1 +1,5 @@
-print("Hello World")
+def multiplication(a,b):
+	return a*b
+a=int(input("input the first number you would like to multiply"))
+b=int(input("input the second number to multiply"))
+print(multiplication(a,b))
